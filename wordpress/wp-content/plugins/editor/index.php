783b0539fcd5d7f432f84babf29883320a2a4585
GuillaumeCerdan <gcerdan83@gmail.com>
2019-03-01 23:13:33 +0100

    Fichier plus inclus en back office + lien admin_bar_menu

--- wordpress/wp-content/plugins/editor/index.php
+++ wordpress/wp-content/plugins/editor/index.php
@@ -1,18 +1,22 @@
 <?php
 /*
-Plugin Name: Getup Editor
+Plugin Name: Redcat Editor
 Description: The first Wordpress smart editor that includes IntelliSense, coloring and more !
 Author: [Getup] Guillaume
 */
 
 add_action('admin_menu','test_plugin_setup_menu');  
 function test_plugin_setup_menu(){
-    add_menu_page('Getup Editor', 'Getup Editor', 'manage_options', 'test-plugin', 'init_editor');
+    add_menu_page('Redcat Editor', 'Getup Editor', 'manage_options', 'test-plugin', 'init_editor');
 }
 
-wp_enqueue_script( 'js', plugin_dir_url( __FILE__ ) . 'js/editor.js', array( 'jquery' ) );
+// Link JS script
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+function enqueue_scripts() {
+  wp_enqueue_script('js', plugin_dir_url( __FILE__ ) . 'js/editor.js', array('jquery'));
+}
 
-function init_editor(){
+function init_editor() {
     $file = plugin_dir_url( __FILE__ ).'js/editor.js';
     $previousCode = file_get_contents($file);
 
@@ -33,7 +37,22 @@ function init_editor(){
     echo($js_setup);
     
 }
- 
+
+
+add_action('admin_bar_menu', 'modify_admin_bar');
+function modify_admin_bar($wp_admin_bar) {
+  $wp_admin_bar->add_menu(array(
+    'id' => 'directLink',
+    'title' => __('Redcat Editor'),
+    'href' => 'http://localhost/editorPlugin/wordpress/wp-admin/admin.php?page=test-plugin',
+    'meta'  => array(
+      'target'=> '_blank',
+      'title' => __('Redcat Editor'),
+    ),
+  ));
+}
+
+
 // Hook the 'wp_footer' action hook, add the function named 'mfp_Add_Text' to it
 //add_action("wp_footer", "mfp_Add_Text");
  
